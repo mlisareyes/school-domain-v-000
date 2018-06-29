f3e0f05a95afa290e4c3f4e71d10b6202538c9ad
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-29 21:55:46 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,24 +1,25 @@
 # code here!
 class School
-  attr_accessor :roster, :student_name, :school_name
+  attr_accessor :name, roster
 
-  def initialize(school_name)
-    @school_name = school_name
+  def initialize(name)
+    @name = name
     @roster = {}
   end
 
   def add_student(student_name, grade)
-    if @roster[grade]
-      @roster[grade] << student_name
-    else
-      @roster[grade] = []
-      @roster[grade] << student_name
-    end
+    roster[grade] = []
+    roster[grade] << student_name
+  end
 
-    def sort
-      sorted = {}
-      @roster.each do |grade, student_name|
-      @roster[grade] = student_name.sort
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |grade,students|
+      sorted[grade] = students.sort
     end
     sorted
   end
